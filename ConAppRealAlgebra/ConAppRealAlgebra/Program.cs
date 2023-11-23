31498690243092a592c02cf84ff0696a2f728c4f
Naveenn789 <naveengalla77@gmail.com>
2023-11-23 15:26:33 +0530

    Intial Commit

---
+++ ConAppRealAlgebra/ConAppRealAlgebra/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShapeAreaLib;
+
+namespace ConAppRealAlgebra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine("Area of the Reactangle is : ");
+            Console.WriteLine(rectangle.Area(10, 20));
+            Square square = new Square();
+            Console.WriteLine("Area of the Square is : ");
+            Console.WriteLine(square.Area(10));
+            Circle circle = new Circle();
+            Console.WriteLine("Area of the Circle is : ");
+            Console.WriteLine(circle.Area(7));
+            Triangle triangle = new Triangle();
+            Console.WriteLine("Area of the Triangle is : ");
+            Console.WriteLine(triangle.Area(20, 30));
+
+            Console.ReadKey();
+
+        }
+    }
+}
